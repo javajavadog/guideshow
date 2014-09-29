08e159289ddcfb89a439c81a9dab785ed1e11126
li.xiang <li.xiang@ttwx365.com>
2014-09-29 16:00:50 +0800

    minor update for add more comments

--- GuideShowDemo/src/com/yishabeila/gsd/ExampleGuideActivity.java
+++ GuideShowDemo/src/com/yishabeila/gsd/ExampleGuideActivity.java
@@ -10,8 +10,8 @@ import com.chechezhi.ui.guide.AbsGuideActivity;
 import com.chechezhi.ui.guide.SingleElement;
 import com.chechezhi.ui.guide.SinglePage;
 
-public class ExampleGuideActivity extends AbsGuideActivity{
-    
+public class ExampleGuideActivity extends AbsGuideActivity {
+
     @Override
     public List<SinglePage> buildGuideContent() {
         // prepare the information for our guide
@@ -19,26 +19,34 @@ public class ExampleGuideActivity extends AbsGuideActivity{
 
         SinglePage page01 = new SinglePage();
         page01.mBackground = getResources().getDrawable(R.drawable.bg_page_01);
-        SingleElement e01 = new SingleElement(200, 200, 400, 400, 0.0f, 1.0f, BitmapFactory.decodeResource(getResources(), R.drawable.ic_stuff));
-        SingleElement e02 = new SingleElement(700, 800, 700, 100, 0.0f, 1.0f, BitmapFactory.decodeResource(getResources(), R.drawable.ic_stuff));
+        SingleElement e01 = new SingleElement(200, 200, 400, 400, 0.0f, 1.0f, BitmapFactory.decodeResource(
+                getResources(), R.drawable.ic_stuff));
+        SingleElement e02 = new SingleElement(700, 800, 700, 100, 0.0f, 1.0f, BitmapFactory.decodeResource(
+                getResources(), R.drawable.ic_stuff));
         page01.mElementsList.add(e01);
         page01.mElementsList.add(e02);
         guideContent.add(page01);
-        
+
         SinglePage page02 = new SinglePage();
-        page02.mBackground = getResources().getDrawable(R.drawable.bg_page_02);    
-        SingleElement e03 = new SingleElement(400, 400, -100, -100, 1.0f, 0.0f, BitmapFactory.decodeResource(getResources(), R.drawable.ic_stuff));
-        SingleElement e04 = new SingleElement(700, 100, 700, -200, 1.0f, 0.0f, BitmapFactory.decodeResource(getResources(), R.drawable.ic_stuff));
+        page02.mBackground = getResources().getDrawable(R.drawable.bg_page_02);
+        SingleElement e03 = new SingleElement(400, 400, -100, -100, 1.0f, 0.0f, BitmapFactory.decodeResource(
+                getResources(), R.drawable.ic_stuff));
+        SingleElement e04 = new SingleElement(700, 100, 700, -200, 1.0f, 0.0f, BitmapFactory.decodeResource(
+                getResources(), R.drawable.ic_stuff));
         page02.mElementsList.add(e03);
         page02.mElementsList.add(e04);
         guideContent.add(page02);
 
         SinglePage page03 = new SinglePage();
         page03.mBackground = getResources().getDrawable(R.drawable.bg_page_03);
-        SingleElement e05 = new SingleElement(-100, 2000, 100, 100, 1.0f, 1.0f, BitmapFactory.decodeResource(getResources(), R.drawable.ic_stuff));
-        SingleElement e06 = new SingleElement(100, 2000, 300, 120, 1.0f, 1.0f, BitmapFactory.decodeResource(getResources(), R.drawable.ic_stuff));
-        SingleElement e07 = new SingleElement(200, 2000, 600, 140, 1.0f, 1.0f, BitmapFactory.decodeResource(getResources(), R.drawable.ic_stuff));
-        SingleElement e08 = new SingleElement(300, 2000, 900, 160, 1.0f, 1.0f, BitmapFactory.decodeResource(getResources(), R.drawable.ic_stuff));
+        SingleElement e05 = new SingleElement(-100, 2000, 100, 100, 1.0f, 1.0f, BitmapFactory.decodeResource(
+                getResources(), R.drawable.ic_stuff));
+        SingleElement e06 = new SingleElement(100, 2000, 300, 120, 1.0f, 1.0f, BitmapFactory.decodeResource(
+                getResources(), R.drawable.ic_stuff));
+        SingleElement e07 = new SingleElement(200, 2000, 600, 140, 1.0f, 1.0f, BitmapFactory.decodeResource(
+                getResources(), R.drawable.ic_stuff));
+        SingleElement e08 = new SingleElement(300, 2000, 900, 160, 1.0f, 1.0f, BitmapFactory.decodeResource(
+                getResources(), R.drawable.ic_stuff));
         page03.mElementsList.add(e05);
         page03.mElementsList.add(e06);
         page03.mElementsList.add(e07);
@@ -47,10 +55,14 @@ public class ExampleGuideActivity extends AbsGuideActivity{
 
         SinglePage page04 = new SinglePage();
         page04.mBackground = getResources().getDrawable(R.drawable.bg_page_04);
-        SingleElement e09 = new SingleElement(100, 100, 3000, 3000, 1.0f, 1.0f, BitmapFactory.decodeResource(getResources(), R.drawable.ic_stuff));
-        SingleElement e10 = new SingleElement(300, 120, 3000, 3000, 1.0f, 1.0f, BitmapFactory.decodeResource(getResources(), R.drawable.ic_stuff));
-        SingleElement e11 = new SingleElement(600, 140, 3000, 3000, 1.0f, 1.0f, BitmapFactory.decodeResource(getResources(), R.drawable.ic_stuff));
-        SingleElement e12 = new SingleElement(900, 160, 3000, 3000, 1.0f, 1.0f, BitmapFactory.decodeResource(getResources(), R.drawable.ic_stuff));
+        SingleElement e09 = new SingleElement(100, 100, 3000, 3000, 1.0f, 1.0f, BitmapFactory.decodeResource(
+                getResources(), R.drawable.ic_stuff));
+        SingleElement e10 = new SingleElement(300, 120, 3000, 3000, 1.0f, 1.0f, BitmapFactory.decodeResource(
+                getResources(), R.drawable.ic_stuff));
+        SingleElement e11 = new SingleElement(600, 140, 3000, 3000, 1.0f, 1.0f, BitmapFactory.decodeResource(
+                getResources(), R.drawable.ic_stuff));
+        SingleElement e12 = new SingleElement(900, 160, 3000, 3000, 1.0f, 1.0f, BitmapFactory.decodeResource(
+                getResources(), R.drawable.ic_stuff));
         page04.mElementsList.add(e09);
         page04.mElementsList.add(e10);
         page04.mElementsList.add(e11);
@@ -60,6 +72,7 @@ public class ExampleGuideActivity extends AbsGuideActivity{
         SinglePage page05 = new SinglePage();
         page05.mCustomFragment = new EntryFragment();
         guideContent.add(page05);
+
         return guideContent;
     }
 
@@ -77,12 +90,18 @@ public class ExampleGuideActivity extends AbsGuideActivity{
     public boolean drawDot() {
         return true;
     }
-    
-    public void entryApp(){
-        // Time to entry your app! We just finish the activity, replace it with your code.
+
+    public void entryApp() {
+        // Time to entry your app! We just finish the activity, replace it with
+        // your code.
+
         finish();
     }
 
+    /**
+     * You need provide an id to the pager. You could define an id in
+     * values/ids.xml and use it.
+     */
     @Override
     public int getPagerId() {
         return R.id.guide_container;
--- GuideShowDemo/src/com/yishabeila/gsd/MainActivity.java
+++ GuideShowDemo/src/com/yishabeila/gsd/MainActivity.java
@@ -6,16 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         findViewById(R.id.btn_start).setOnClickListener(new OnClickListener() {
-            
+
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, ExampleGuideActivity.class);
